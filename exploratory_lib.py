8a20ae0a6b1c3ac33ccbbb6936d98d373e808092
Rolf Traeger Brautigam <rolftb@gmail.com>
2023-04-04 19:58:01 -0400

    Carga de las librerias, de transformacion

--- exploratory_lib.py
+++ exploratory_lib.py
@@ -61,3 +61,76 @@ class class_diccionarios:
         return self.name_dic
     def dic_numbers(self):
         return self.int_dic
+
+# ================================
+# Transformacion de los datos
+# ================================
+def convert_to_date(datetime_str):
+    return pd.to_datetime(datetime_str, format='%Y-%m-%d %H:%M:%S')
+
+# HIGH_SEASON
+def col_high_season(date_x):
+    """
+    Retorna un binario 1 en el caso que la fecha sea de temporada alta 
+    0 en el caso de un escenario normal.
+    """
+    Año_actual= date_x.year
+    # Between Dec-15 and Mar-3
+    verano_start = convert_to_date( f"{Año_actual}-12-15 00:00:00") 
+    verano_end = convert_to_date( f"{Año_actual}-3-4 00:00:00")
+    bool_verano = (
+            (verano_end > date_x )
+            |(date_x >verano_start)
+            )
+    # Jul-15 and Jul-31, 
+    invierno_start = convert_to_date( f"{Año_actual}-7-15 00:00:00") 
+    invierno_end = convert_to_date( f"{Año_actual}-8-1 00:00:00")
+    bool_invierno = (invierno_end > date_x >invierno_start)
+    # Sep-11 and Sep-30
+    sep_start = convert_to_date( f"{Año_actual}-9-11 00:00:00") 
+    sep_end = convert_to_date( f"{Año_actual}-10-1 00:00:00")
+    bool_sep = (sep_end > date_x >sep_start)
+    if bool_verano | bool_invierno | bool_sep:
+        return 1
+    else:
+        return 0
+def period_day_func(hora_in):
+    """
+    morning (between 5:00 and 11:59)
+    afternoon (between 12:00 and 18:59) 
+    night (between 19:00 and 4:59)
+    """
+    if 5<=hora_in<12:
+        return "morning"
+    elif 12<=hora_in<19:
+        return "afternoon"
+    elif (19<=hora_in) | (hora_in <5):
+        return "night"
+    else:
+        return "error"
+
+# Transforma los datos y entrega la fecha
+def additional_columns(df_in,col_name_i ="Fecha-I",col_name_0 ="Fecha-O"):
+    # ==============
+    # HIGH_SEASON
+    # 1 if Date-I is between Dec-15 and Mar-3, or Jul-15 and Jul-31, or Sep-11 and Sep-30, 0 otherwise.
+    # ==============
+    # col_name_i ="Fecha-I"
+    # col_name_0 ="Fecha-O"
+    df_out = df_in.copy()
+    df_out["date_i"] = df_out[col_name_i].apply(convert_to_date)
+    df_out["date_o"] = df_out[col_name_0].apply(convert_to_date)
+    df_out["high_season"] = df_out["date_i"].apply(col_high_season)
+    # ==============
+    # PERIOD_DAY
+    # ==============
+    df_out["period_day"] = df_out["date_i"].dt.hour.apply(period_day_func)
+    # =====================
+    # MIN_DIFF
+    # =====================
+    df_out["min_diff"] = (df_out["date_o"] - df_out["date_i"]).dt.total_seconds() / 60
+    # =====================
+    # delay_15
+    # =====================
+    df_out["delay_15"] = df_out["min_diff"].apply( lambda x: 1 if x >15 else 0 )
+    return df_out
